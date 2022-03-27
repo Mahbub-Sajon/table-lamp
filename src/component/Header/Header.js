81fa8069c07b5624b260ca7355de7c2b053ac1f0
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-03-27 12:57:59 +0600

    header added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='headline'>
+            <h1>Please Buy a Lamp!!!</h1>
+            <h2>Choose Any 4 Lamps!!!</h2>
         </div>
     );
 };
